2b8d6c665091f5759908a25a27972b0b41b42417
Tanishq Shaikh <tanishq.s@somaiya.edu>
2024-04-06 12:57:52 -0400

    comments and prettify

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,38 +1,52 @@
+// AWS Secret manager initializations
 const secretName = 'weatherApp';
-const { getSecretValue } = require('./config/aws');
+const {
+    getSecretValue
+} = require('./config/aws');
 
+// Express
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('mongoose');
-const { Schema } = mongodb;
-
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8888;
 
-getSecretValue(secretName)
-  .then(secret => {
-    // Now you can access the secret object
-    console.log('Secret:', secret);
-    // Example: const apiKey = secret.API_KEY;
-    // Continue with your application logic here
-  })
-  .catch(error => {
-    console.error('Error fetching secret:', error);
-    // Handle error appropriately
-  });
+// Mongo DB initializations
+const mongodb = require('mongoose');
+const {
+    Schema
+} = mongodb;
 
+// function for fetching secrets from AWS Secrets Manager
+getSecretValue(secretName)
+    .then(secret => {
+        // console.log('Secret:', secret);
+    })
+    .catch(error => {
+        console.error('Error fetching secret:', error);
+    });
+
+// function for formatting weather data
 const formatWeatherData = (weatherData) => {
-  return {
-    city: weatherData.name,
-    temperature: weatherData.main.temp,
-    description: weatherData.weather[0].description,
-  };
+    return {
+        city: weatherData.name,
+        temperature: weatherData.main.temp,
+        description: weatherData.weather[0].description,
+    };
 };
 
 const weatherSchema = new Schema({
-  city: { type: String, unique: true },
-  data: { type: Object },
-  createdAt: { type: Date, default: Date.now, expires: 60 * 30 }
+    city: {
+        type: String,
+        unique: true
+    },
+    data: {
+        type: Object
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 60 * 30
+    }
 });
 
 const Weather = mongodb.model('Weather', weatherSchema);
@@ -41,55 +55,63 @@ app.use(bodyParser.json());
 
 app.get('/api/weather', async (req, res) => {
 
-  // Fetching the secrets and setting variables
-  const secrets = await getSecretValue(secretName);
-  const api_key = secrets.API_KEY;
-  const username = secrets.MONGODB_USERNAME;
-  const password = secrets.MONGODB_PASSWORD;
+    // Fetching the secrets and setting variables
+    const secrets = await getSecretValue(secretName);
+    const api_key = secrets.API_KEY;
+    const username = secrets.MONGODB_USERNAME;
+    const password = secrets.MONGODB_PASSWORD;
 
-  const uri = `mongodb+srv://${username}:${password}@weathercluster.uacqtze.mongodb.net/?retryWrites=true&w=majority&appName=weatherCluster`;
+    // URI for connecting to mongo
+    const uri = `mongodb+srv://${username}:${password}@weathercluster.uacqtze.mongodb.net/?retryWrites=true&w=majority&appName=weatherCluster`;
+    mongodb.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const db = mongodb.connection;
 
-  mongodb.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  const db = mongodb.connection;
+    // API for hitting the open weather API
+    const cityName = req.query.cityName;
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api_key}`;
 
-  const cityName = req.query.cityName;
-  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api_key}`;
+    try {
 
-  try {
+        const cachedWeather = await Weather.findOne({
+            city: cityName
+        });
 
-    const cachedWeather = await Weather.findOne({ city: cityName });
-    if(cachedWeather) {
-      console.log('Weather data found in cache' + JSON.stringify(cachedWeather.data.name));
+        // Checking the cache before hitting API
+        if (cachedWeather) {
+            const weatherData = formatWeatherData(cachedWeather.data);
+            res.set('Access-Control-Allow-Origin', '*');
+            return res.json(weatherData);
+        } else {
 
-      const weatherData = formatWeatherData(cachedWeather.data);
+            const fetch = await import('node-fetch');
+            const response = await fetch.default(apiUrl);
+            const data = await response.json();
 
-      console.log("weatherData " + JSON.stringify(weatherData));
-      res.set('Access-Control-Allow-Origin', '*');
-      return res.json(weatherData);
-    } else {
+            await Weather.create({
+                city: cityName,
+                data: data
+            });
 
-    const fetch = await import('node-fetch');
-    const response = await fetch.default(apiUrl);
-    const data = await response.json();
+            if (!response.ok) {
+                throw new Error('City not found');
+            }
 
-    await Weather.create({ city: cityName, data: data });
-
-    if (!response.ok) {
-      throw new Error('City not found');
+            const weatherData = formatWeatherData(data);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.json(weatherData);
+        }
+    } catch (error) {
+        res.status(404).json({
+            error: "Enter a valid city!"
+        });
     }
-    
 
-    const weatherData = formatWeatherData(data);
-    res.set('Access-Control-Allow-Origin', '*');
-    res.json(weatherData);
-  }
-  } catch (error) {
-    res.status(404).json({ error: "Enter a valid city!" });
-  }
-  
 });
 
 // Server start
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- src/frontend/home.js
+++ src/frontend/home.js
@@ -1,63 +1,66 @@
-import React, { useState } from 'react';
-import DisplayWeather from './DisplayWeatherComponent';
+import React, {
+    useState
+} from 'react';
 
 const Home = () => {
-    const [cityName, setCityName] = useState('');
-    const [weather, setWeather] = useState('');
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [dataLoaded, setDataLoaded] = useState(false);
+        const [cityName, setCityName] = useState('');
+        const [weather, setWeather] = useState('');
+        const [error, setError] = useState('');
+        const [isLoading, setIsLoading] = useState(false);
+        const [dataLoaded, setDataLoaded] = useState(false);
 
-    const handleSubmitEvent = async (event) => {
-        setDataLoaded(false);
-        event.preventDefault();
-        setIsLoading(true);
-        setError('');
+        const handleSubmitEvent = async (event) => {
+            setDataLoaded(false);
+            event.preventDefault();
+            setIsLoading(true);
+            setError('');
 
-        try {
-            const response = await fetch(`http://localhost:8888/api/weather?cityName=${cityName}`);
+            try {
+                const response = await fetch(`http://localhost:8888/api/weather?cityName=${cityName}`);
 
-            const data = await response.json();
-            setWeather(data);
-            setDataLoaded(true);
+                const data = await response.json();
+                setWeather(data);
+                setDataLoaded(true);
 
-        } catch (error) {
-            console.log(JSON.stringify(error));
-            setError(error.message);
-        } finally {
-            setIsLoading(false);
+            } catch (error) {
+                console.log(JSON.stringify(error));
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
         }
-    }
 
-    const handleInputChange = (event) => {
-        setCityName(event.target.value);
-    }
+        const handleInputChange = (event) => {
+            setCityName(event.target.value);
+        }
 
-    return (
-        <div>
-            <h1>Weather Data</h1>
-            <input
-              type="text"
-              placeholder="Enter city name"
-              value={cityName}
-              onChange={handleInputChange} />
-            
-            <button type="text" onClick = {handleSubmitEvent}>Get Weather</button>
-          {<br/>}
-          {error && <p>{error}</p>}
-
-          {isLoading ? (
-            <h2>Loading...</h2>
-            ) : dataLoaded && weather ? (
-                <div>
-                    <h2>City: {weather.city}</h2>
-                    <h2>Temperature: {weather.temperature}</h2>
-                    <h2>Description: {weather.description}</h2>
-                </div>
-            ) : null }
-          
-        </div>
-      );
-};
+        return (
+            <div>
+                <h1>Weather Data</h1>
+                <input
+                  type="text"
+                  placeholder="Enter city name"
+                  value={cityName}
+                  onChange={handleInputChange} />
+                
+                <button type="text" onClick = {handleSubmitEvent}>Get Weather</button>
+              {<br/>}
+              {error && <p>{error}</p>}
+        
+              {isLoading ? (
+                <h2>Loading...</h2>
+                ) : dataLoaded && weather ? (
+                    <div>
+                        <h2>City: {weather.city}</h2>
+                        <h2>Temperature: {weather.temperature}</h2>
+                        <h2>Description: {weather.description}</h2>
+                    </div>
+                ) : null }
+              
+            </div>
+          );
+        
+        };
 
 export default Home;
+
